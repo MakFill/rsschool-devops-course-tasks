1ba41953fab323acf652baf0e633e5cc79c5e669
MakFil <makfil1177@gmail.com>
2024-10-12 12:52:26 +0200

    Remove unnecessary resources

--- variables.tf
+++ variables.tf
@@ -3,11 +3,6 @@ variable "region" {
   type        = string
 }
 
-variable "bucket_name" {
-  description = "AWS s3 bucket name"
-  type        = string
-}
-
 variable "AWS_ARN" {
   description = "AWS ARN"
   type        = string
