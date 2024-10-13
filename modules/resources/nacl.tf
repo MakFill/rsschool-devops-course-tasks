7665894b371c8de71789022e09141e374b0e038a
MakFil <makfil1177@gmail.com>
2024-10-13 17:38:07 +0200

    Fix nack rule number issue

--- modules/resources/nacl.tf
+++ modules/resources/nacl.tf
@@ -28,26 +28,26 @@ resource "aws_network_acl_rule" "public_inbound_https" {
   to_port        = 443
 }
 
-resource "aws_network_acl_rule" "public_inbound_ssh" {
-  network_acl_id = aws_network_acl.public.id
-  rule_number    = 120
-  protocol       = "tcp"
-  rule_action    = "allow"
-  egress         = false
-  cidr_block     = var.IP
-  from_port      = 22
-  to_port        = 22
-}
-
 resource "aws_network_acl_rule" "public_inbound_all_internal" {
   network_acl_id = aws_network_acl.public.id
-  rule_number    = 130
+  rule_number    = 120
   protocol       = "-1"
   rule_action    = "allow"
   egress         = false
   cidr_block     = aws_vpc.main_vpc.cidr_block
   from_port      = 0
   to_port        = 0
+}
+
+resource "aws_network_acl_rule" "public_inbound_ssh" {
+  network_acl_id = aws_network_acl.public.id
+  rule_number    = 130
+  protocol       = "tcp"
+  rule_action    = "allow"
+  egress         = false
+  cidr_block     = var.IP
+  from_port      = 22
+  to_port        = 22
 }
 
 # Egress rules for public NACL
