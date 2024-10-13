8771869db2d96fff495835b1da248cb43b580ea4
MakFil <makfil1177@gmail.com>
2024-10-13 15:26:22 +0200

    Add ec2 tag

--- modules/resources/ec2.tf
+++ modules/resources/ec2.tf
@@ -3,4 +3,8 @@ resource "aws_instance" "private_instance" {
   instance_type = var.bastion_instance_type
   subnet_id = aws_subnet.private_subnets[0].id
   vpc_security_group_ids = [aws_security_group.private_sg.id]
+
+  tags = {
+    Name = "Example ec2 instance to connect with Bastion Host"
+  }
 }
