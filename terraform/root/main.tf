d563df07675260e503203a32eab4e79471cc526a
MakFil <makfil1177@gmail.com>
2024-10-19 14:12:47 +0200

    Test pipeline clean

--- terraform/root/main.tf
+++ terraform/root/main.tf
@@ -6,47 +6,47 @@ terraform {
   }
 }
 
-module "vpc" {
-  source              = "../modules/vpc"
+# module "vpc" {
+#   source              = "../modules/vpc"
 
-  IP                  = var.IP
-  default_cidr        = var.default_cidr
-}
+#   IP                  = var.IP
+#   default_cidr        = var.default_cidr
+# }
 
-module "sg" {
-  source              = "../modules/sg"
+# module "sg" {
+#   source              = "../modules/sg"
 
-  IP                  = var.IP
-  default_cidr        = var.default_cidr
-  main_vpc_id         = module.vpc.main_vpc_id
-  main_vpc_cidr_block = module.vpc.main_vpc_cidr_block
-}
+#   IP                  = var.IP
+#   default_cidr        = var.default_cidr
+#   main_vpc_id         = module.vpc.main_vpc_id
+#   main_vpc_cidr_block = module.vpc.main_vpc_cidr_block
+# }
 
-module "iam" {
-  source              = "../modules/iam"
+# module "iam" {
+#   source              = "../modules/iam"
 
-  AWS_ARN             = var.AWS_ARN
-  REPO                = var.REPO
-}
+#   AWS_ARN             = var.AWS_ARN
+#   REPO                = var.REPO
+# }
 
-module "bastion_host" {
-  source              = "../modules/bastion_host"
+# module "bastion_host" {
+#   source              = "../modules/bastion_host"
 
-  ec2_ami             = var.ec2_ami
-  ec2_instance_type   = var.ec2_instance_type
-  key_pair            = var.key_pair
-  public_subnet_id    = module.vpc.public_subnet_1_id
-  bastion_sg_id       = module.sg.bastion_sg_id
-}
+#   ec2_ami             = var.ec2_ami
+#   ec2_instance_type   = var.ec2_instance_type
+#   key_pair            = var.key_pair
+#   public_subnet_id    = module.vpc.public_subnet_1_id
+#   bastion_sg_id       = module.sg.bastion_sg_id
+# }
 
-module "ec2" {
-  source              = "../modules/ec2"
+# module "ec2" {
+#   source              = "../modules/ec2"
 
-  ec2_ami             = var.ec2_ami
-  ec2_instance_type   = var.ec2_instance_type
-  key_pair            = var.key_pair
-  public_subnet_id    = module.vpc.public_subnet_2_id
-  private_subnet_id   = module.vpc.private_subnet_1_id
-  public_sg_id        = module.sg.public_sg_id
-  private_sg_id       = module.sg.private_sg_id
-}
+#   ec2_ami             = var.ec2_ami
+#   ec2_instance_type   = var.ec2_instance_type
+#   key_pair            = var.key_pair
+#   public_subnet_id    = module.vpc.public_subnet_2_id
+#   private_subnet_id   = module.vpc.private_subnet_1_id
+#   public_sg_id        = module.sg.public_sg_id
+#   private_sg_id       = module.sg.private_sg_id
+# }
