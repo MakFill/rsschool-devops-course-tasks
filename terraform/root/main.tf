1fe64d88fc46025f8f088aa23920b76161a2e6b2
MakFil <makfil1177@gmail.com>
2024-10-19 11:40:17 +0200

    Fix modules paths

--- terraform/root/main.tf
+++ terraform/root/main.tf
@@ -7,28 +7,28 @@ terraform {
 }
 
 module "vpc" {
-  source            = "./modules/vpc"
+  source            = "../modules/vpc"
 
   IP                = var.IP
   default_cidr      = var.default_cidr
 }
 
 module "sg" {
-  source            = "./modules/sg"
+  source            = "../modules/sg"
 
   IP                = var.IP
   default_cidr      = var.default_cidr
 }
 
 module "iam" {
-  source            = "./modules/sg"
+  source            = "../modules/iam"
 
   AWS_ARN           = var.AWS_ARN
   REPO              = var.REPO
 }
 
 module "bastion_host" {
-  source            = "./modules/bastion_host"
+  source            = "../modules/bastion_host"
 
   ec2_ami           = var.ec2_ami
   ec2_instance_type = var.ec2_instance_type
@@ -36,7 +36,7 @@ module "bastion_host" {
 }
 
 module "ec2" {
-  source            = "./modules/ec2"
+  source            = "../modules/ec2"
 
   ec2_ami           = var.ec2_ami
   ec2_instance_type = var.ec2_instance_type
