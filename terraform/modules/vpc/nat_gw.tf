ad8e92acce61e0c63424b0be486ed50637803fbb
MakFil <makfil1177@gmail.com>
2024-10-19 14:52:40 +0200

    Remove nat and ig depends_on fields

--- terraform/modules/vpc/nat_gw.tf
+++ terraform/modules/vpc/nat_gw.tf
@@ -2,7 +2,6 @@ resource "aws_eip" "nat_eip" {
 }
 
 resource "aws_nat_gateway" "nat" {
-  depends_on    = [aws_internet_gateway.igw]
   allocation_id = aws_eip.nat_eip.id
   subnet_id     = aws_subnet.public_subnets[0].id 
 }
