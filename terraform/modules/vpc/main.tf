dd694f534a38fb56305060906a0ddbdf70483478
MakFil <makfil1177@gmail.com>
2024-10-20 11:50:05 +0200

    enable dns hostnames and enable dns support for VPC

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,9 +1,11 @@
 resource "aws_vpc" "main_vpc" {
-  cidr_block = var.main_vpc_cidr_block
+  cidr_block           = var.main_vpc_cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id     = aws_vpc.main_vpc.id
+  vpc_id               = aws_vpc.main_vpc.id
 
-  depends_on = [aws_vpc.main_vpc]
+  depends_on           = [aws_vpc.main_vpc]
 }
